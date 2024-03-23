9c3d581824269bfa7f8d2d1ed4011f2021342937
usmonjon <umediagram@gmail.com>
2024-03-23 17:42:56 +0500

    Setup project

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import "./app.css";
+
+const App = () => {
+  return (
+    <div>
+      Hello world!
+    </div>
+  )
+}
+
+export default App
